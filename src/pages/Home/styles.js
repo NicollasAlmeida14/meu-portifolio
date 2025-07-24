1e06f523a379bb6bc26216d730889507a154af1b
nicollasalmeida14 <nicollasalmeida1908@gmail.com>
2025-07-24 11:35:12 -0300

    Correção na largura da tela

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -14,6 +14,7 @@ export const Container = styled.div`
     background: linear-gradient(90deg,rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
     color: #fff;
     min-height: 100vh;
+    max-width: 100vw;
 `
 
 export const Main = styled.main`
